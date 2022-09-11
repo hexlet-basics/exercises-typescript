98f3c94d4d0f4e45df58655091380cef3ac580c1
Kirill Mokevnin <mokevnin@gmail.com>
2022-09-11 11:07:55 -0400

    update functions/function-as-parameter
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- modules/20-functions/10-function-as-parameter/index.ts
+++ modules/20-functions/10-function-as-parameter/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+// BEGIN
+function filter(numbers: number[], callback: (n: number) => boolean) {
+  const result: number[] = [];
+  numbers.forEach((n) => {
+    if (callback(n)) {
+      result.push(n);
+    }
+  });
+  return result;
+}
+// END
+
+export default filter;
--- modules/20-functions/10-function-as-parameter/test.ts
+++ modules/20-functions/10-function-as-parameter/test.ts
@@ -1,3 +1,12 @@
-test('function', () => {
-});
+import filter from './index';
 
+test('function', () => {
+  const result = filter([], (n) => n > 3);
+  expect(result).toEqual([]);
+
+  const result2 = filter([3, 2, 8, 9], (n) => n > 3);
+  expect(result2).toEqual([8, 9]);
+
+  const result3 = filter([3, 2, 8, 9], (n) => n < 8);
+  expect(result3).toEqual([3, 2]);
+});
