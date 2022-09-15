eb7d2708e730a7ff4fe90b70e1e348c08d39c6e8
Victor Kasap <kasap.victor@gmail.com>
2022-09-15 18:28:17 +0300

    fix: Fixed test for 45-anonymous-functions

--- modules/10-basics/45-anonymous-functions/index.ts
+++ modules/10-basics/45-anonymous-functions/index.ts
@@ -1,6 +1,5 @@
 // BEGIN
-function getEvenNumbers() {
-  const numbers = [1, 3, 8, 9, 100, 23, 55, 34];
+function getEvenNumbers(numbers: number[]) {
   const evenNumbers = numbers.filter((n) => n % 2 === 0);
   return evenNumbers;
 }
