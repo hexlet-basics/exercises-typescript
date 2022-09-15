6c615e2d44618ca03b7c85ac114761215896e87f
Victor Kasap <73124371+kasapvictor@users.noreply.github.com>
2022-09-15 18:17:35 +0300

    Update test function
    Чуть чуть поправил функцию для тестов, чтоб не править описание и любой массив с числами можно было передавать

--- modules/10-basics/45-anonymous-functions/test.ts
+++ modules/10-basics/45-anonymous-functions/test.ts
@@ -1,5 +1,5 @@
 import getEvenNumbers from './index';
 
 test('function', () => {
-  expect(getEvenNumbers()).toEqual([8, 100, 34]);
+  expect(getEvenNumbers([1, 3, 12, 30, 43, 54, 68, 97])).toEqual([12, 30, 54, 68]);
 });
