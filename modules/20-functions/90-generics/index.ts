abbc911e20899f1c9148a03184520326395b7575
Kirill Mokevnin <mokevnin@gmail.com>
2022-09-16 21:50:43 -0400

    update content
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- modules/20-functions/90-generics/index.ts
+++ modules/20-functions/90-generics/index.ts
@@ -1,1 +1,5 @@
-console.log('Hello, World!');
+function last<T>(coll: Array<T>): T | null {
+  return coll.length > 0 ? coll[coll.length - 1] : null;
+}
+
+export default last;
--- modules/20-functions/90-generics/test.ts
+++ modules/20-functions/90-generics/test.ts
@@ -1,3 +1,7 @@
-test('function', () => {
-});
+import last from './index';
 
+test('function', () => {
+  expect(last([])).toBe(null);
+  expect(last([3, 4])).toBe(4);
+  expect(last(['cat', 'dog'])).toBe('dog');
+});
