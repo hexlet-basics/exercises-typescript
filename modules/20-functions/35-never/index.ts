5080ae7676d75f1dd3863054b40b55e7af049aa2
Kirill Mokevnin <mokevnin@gmail.com>
2022-09-26 14:27:08 -0400

    update content
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- modules/20-functions/35-never/index.ts
+++ modules/20-functions/35-never/index.ts
@@ -1,9 +1,7 @@
 // BEGIN
-function forEach(numbers: number[], callback: (n: any, index: number) => void) {
-  const result: number[] = [];
-  numbers.forEach((n, index) => callback(n, index));
-  return result;
+function fail(): never {
+  throw new Error('wow');
 }
 // END
 
-export default forEach;
+export default fail;
--- modules/20-functions/35-never/test.ts
+++ modules/20-functions/35-never/test.ts
@@ -1,15 +1,5 @@
-import forEach from './index';
+import fail from './index';
 
 test('function', () => {
-  const result: number[] = [];
-  forEach([], (n) => result.push(n));
-  expect(result).toEqual([]);
-
-  const result2: number[] = [];
-  forEach([3, 9], (n) => result2.push(n + 1));
-  expect(result2).toEqual([4, 10]);
-
-  const result3: number[] = [];
-  forEach([8, 9], (n, i) => result3.push(n + i));
-  expect(result3).toEqual([8, 10]);
+  expect(fail).toThrow();
 });
