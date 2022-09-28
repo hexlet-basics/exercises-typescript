a81b34d44a9e5c9734a5943ac9cf98839751b86c
Kirill Mokevnin <mokevnin@gmail.com>
2022-09-27 22:01:35 -0400

    update content
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- modules/30-generics/30-generic-functions/index.ts
+++ modules/30-generics/30-generic-functions/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello, World!');
+// BEGIN
+type MyArray<T> = {
+  items: Array<T>;
+  push(value: T): number;
+  filter(callback: (value: T, index: number) => boolean): MyArray<T>;
+};
+// END
+
+export default MyArray;
