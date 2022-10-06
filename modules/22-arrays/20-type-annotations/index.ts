cb20268c879f21672e5001e61bfa28b2431324a1
Kirill Mokevnin <mokevnin@gmail.com>
2022-10-06 18:03:47 -0400

    update content
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- modules/22-arrays/20-type-annotations/index.ts
+++ modules/22-arrays/20-type-annotations/index.ts
@@ -1,5 +1,7 @@
+// BEGIN
 function compact(items: (number | null)[]) {
   return items.filter((item) => item !== null);
 }
+// END
 
 export default compact;
--- modules/22-arrays/50-tuples/index.ts
+++ modules/22-arrays/50-tuples/index.ts
@@ -1,7 +1,9 @@
+// BEGIN
 export type Point = [number, number, number];
 
 function isTheSamePoint(p1: Point, p2: Point): boolean {
   return p1[0] === p2[0] && p1[1] === p2[1] && p1[2] === p2[2];
 }
+// END
 
 export default isTheSamePoint;
