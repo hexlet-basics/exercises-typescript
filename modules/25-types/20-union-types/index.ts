8fc4b370152ed1280eee9ad91ea2cf48136857ab
bondiano <batbondik0@gmail.com>
2022-10-07 17:50:11 +0600

    Add Union Types lesson

--- modules/25-types/20-union-types/index.ts
+++ modules/25-types/20-union-types/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello, World!');
+// BEGIN
+function lastIndex(str: string, char: string): number | null {
+  const index = str.lastIndexOf(char);
+  return index === -1 ? null : index;
+}
+// END
+
+export default lastIndex;
