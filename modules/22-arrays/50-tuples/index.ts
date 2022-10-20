d423e62690f1c7eac38f2bc81da35ee744e72e6f
Sergei Tereshkov <rat87@mail.ru>
2022-10-20 16:12:35 +0300

    change solution

--- modules/22-arrays/50-tuples/index.ts
+++ modules/22-arrays/50-tuples/index.ts
@@ -2,7 +2,7 @@
 export type Point = [number, number, number];
 
 function isTheSamePoint(p1: Point, p2: Point): boolean {
-  return p1[0] === p2[0] && p1[1] === p2[1] && p1[2] === p2[2];
+  return p1.every((el, i) => el === p2[i]);
 }
 // END
 
