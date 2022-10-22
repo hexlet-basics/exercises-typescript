83073893d76f92634cbcba1c01a418499e1ac4cf
bondiano <batbondik0@gmail.com>
2022-10-22 14:52:18 +0600

    update structual typing content

--- modules/25-types/20-union-types/test.ts
+++ modules/25-types/20-union-types/test.ts
@@ -1,6 +1,11 @@
+import { assert } from 'type-assertions';
+import type { Equal } from 'type-assertions';
+
 import lastIndex from './index';
 
 test('lastIndex', () => {
+  assert<Equal<ReturnType<typeof lastIndex>, number | null>>();
+
   const str = 'test';
   expect(lastIndex(str, 't')).toBe(3);
   expect(lastIndex(str, 'e')).toBe(1);
