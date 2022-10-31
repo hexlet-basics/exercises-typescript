479bbb1545028c4a710eeae67cd4b2b809621dab
feycot <feycot@gmail.com>
2022-11-01 02:56:57 +0500

    fix lint

--- modules/10-basics/90-modules/index.ts
+++ modules/10-basics/90-modules/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 // BEGIN
 namespace Company {
   export function isEmployeeEmail(email: string, domain: string) {
@@ -8,16 +9,14 @@ namespace Company {
 
 type User = {
   email: string
-}
+};
 
 function authorize(user: User | null) {
-  if (user == null) {
-    return false;
-  }
-
   const companyDomain = 'hexlet.io';
 
-  return Company.isEmployeeEmail(user.email, companyDomain);
+  const email = user?.email ?? '';
+
+  return Company.isEmployeeEmail(email, companyDomain);
 }
 
 export default authorize;
