4b729fd7f53b4014b3413633d105ad768408b4ed
feycot <feycot@gmail.com>
2022-11-01 02:50:56 +0500

    add 10-basics/90-modules exercise

--- modules/10-basics/90-modules/index.ts
+++ modules/10-basics/90-modules/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello, World!');
+// BEGIN
+namespace Company {
+  export function isEmployeeEmail(email: string, domain: string) {
+    return email.includes(domain);
+  }
+}
+// END
+
+type User = {
+  email: string
+}
+
+function authorize(user: User | null) {
+  if (user == null) {
+    return false;
+  }
+
+  const companyDomain = 'hexlet.io';
+
+  return Company.isEmployeeEmail(user.email, companyDomain);
+}
+
+export default authorize;
--- modules/10-basics/90-modules/test.ts
+++ modules/10-basics/90-modules/test.ts
@@ -1,2 +1,7 @@
+import authorize from './index';
+
 test('function', () => {
+  expect(authorize({ email: 'test@hexlet.io' })).toBe(true);
+  expect(authorize({ email: 'test@example.com' })).toBe(false);
+  expect(authorize(null)).toBe(false);
 });
