a0aa0e8405353cd0af17937cc015d5eb6c5ee5a6
mbredikhin <max11298@yandex.ru>
2023-01-04 21:44:01 +0600

    Add keyof lesson content

--- modules/60-working-with-types/10-keyof/index.ts
+++ modules/60-working-with-types/10-keyof/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+// BEGIN
+function fromEntries<
+  K extends number | string,
+  V extends number | string | boolean | null,
+>(entries: [K, V][]) {
+  return entries.reduce(
+    (acc, [key, value]) => ({ ...acc, [key]: value }),
+    {} as { [Key in K]: V },
+  );
+}
+// END
+
+export default fromEntries;
--- modules/60-working-with-types/10-keyof/test.ts
+++ modules/60-working-with-types/10-keyof/test.ts
@@ -1,3 +1,18 @@
-test('function', () => {
-  expect(1).toBe(1);
+import fromEntries from './index';
+
+test('fromEntries', () => {
+  expect(fromEntries([])).toEqual({});
+  expect(fromEntries([[0, 0]])).toEqual({ 0: 0 });
+  expect(
+    fromEntries([
+      [0, false],
+      [1, true],
+    ]),
+  ).toEqual({ 0: false, 1: true });
+  expect(
+    fromEntries([
+      ['name', 'Vincent'],
+      ['job', 'painter'],
+    ]),
+  ).toEqual({ name: 'Vincent', job: 'painter' });
 });
