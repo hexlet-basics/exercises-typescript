84cf3d40803dddf17c4bae55142be94b1b2c4525
mbredikhin <max11298@yandex.ru>
2023-01-04 15:46:41 +0600

    Add intersection types instructions

--- modules/25-types/30-intersection-types/index.ts
+++ modules/25-types/30-intersection-types/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello, World!');
+// BEGIN
+function intersection<T extends number | string | boolean | null>(
+  ...arrays: T[][]
+): T[] {
+  const [first, second, ...rest] = arrays;
+  if (!second) {
+    return Array.from(new Set(first));
+  }
+  if (!rest.length) {
+    return Array.from(new Set(first.filter((el) => second.includes(el))));
+  }
+  return intersection(
+    first.filter((el) => second.includes(el)),
+    ...rest,
+  );
+}
+// END
+
+export default intersection;
--- modules/25-types/30-intersection-types/test.ts
+++ modules/25-types/30-intersection-types/test.ts
@@ -1,3 +1,23 @@
-test('function', () => {
-  expect(1).toBe(1);
+import intersection from './index';
+
+test('intersection', () => {
+  expect(intersection([])).toEqual([]);
+  expect(intersection([0, true, false, null, 'str'])).toEqual([
+    0,
+    true,
+    false,
+    null,
+    'str',
+  ]);
+  expect(intersection([0, true, false, null, 'str'], [0, false], [])).toEqual(
+    [],
+  );
+  expect(intersection([0, true, false, null, 'str'], [0, false])).toEqual([
+    0,
+    false,
+  ]);
+  expect(
+    intersection([0, true, false, null, 'str'], [0, true, false, null, 'str']),
+  ).toEqual([0, true, false, null, 'str']);
+  expect(intersection([0, null], [1, null], [2, null])).toEqual([null]);
 });
