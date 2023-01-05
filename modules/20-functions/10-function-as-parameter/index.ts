01c151dfd3563e88082268dd51d8a4a31079cf89
Alexander <41731334+qweered@users.noreply.github.com>
2023-01-05 17:05:24 +0300

    revert better solution, improve descriptions (#1)

--- modules/20-functions/10-function-as-parameter/index.ts
+++ modules/20-functions/10-function-as-parameter/index.ts
@@ -1,6 +1,12 @@
 // BEGIN
 function filter(numbers: number[], callback: (n: number) => boolean): number[] {
-  return numbers.filter(callback)
+  const result: number[] = [];
+  numbers.forEach((n) => {
+    if (callback(n)) {
+      result.push(n);
+    }
+  });
+  return result;
 }
 // END
 
