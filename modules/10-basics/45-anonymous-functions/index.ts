715e6b9e3de40b7ac3b285b0fce8fc7aa94898e5
bondiano <batbondik0@gmail.com>
2023-01-09 10:47:42 +0600

    fixes by students comment

--- modules/10-basics/45-anonymous-functions/index.ts
+++ modules/10-basics/45-anonymous-functions/index.ts
@@ -1,7 +1,9 @@
+const numbers = [1, 3, 8, 9, 100, 23, 55, 34];
+
 // BEGIN
 function getEvenNumbers(): number[] {
-  const numbers = [1, 3, 8, 9, 100, 23, 55, 34];
   const evenNumbers = numbers.filter((n) => n % 2 === 0);
+
   return evenNumbers;
 }
 // END
--- modules/20-functions/90-type-narrowing/index.ts
+++ modules/20-functions/90-type-narrowing/index.ts
@@ -1,7 +1,7 @@
 // BEGIN
 function last(value: string | number): string | number {
   if (typeof value === 'number') {
-    return Number(String(value).at(-1));
+    return value % 10;
   }
 
   return value.at(-1) ?? '';
