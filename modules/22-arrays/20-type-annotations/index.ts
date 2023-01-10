0a3e0895de1800cc4e0737ec89f2653f334ba67b
Victor Zhuravlev <mocambe@mail.ru>
2023-01-10 09:04:27 +0300

    Fix linter issue

--- modules/22-arrays/20-type-annotations/index.ts
+++ modules/22-arrays/20-type-annotations/index.ts
@@ -4,7 +4,7 @@ const init: (number | string)[] = [];
 function uniq(coll: (number | string)[]): (number | string)[] {
   return coll.reduce(
     (acc, curr) => (acc.includes(curr) ? acc : [...acc, curr]),
-    init
+    init,
   );
 }
 // END
