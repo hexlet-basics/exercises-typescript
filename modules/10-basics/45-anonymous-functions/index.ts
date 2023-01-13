e5ba131bdb213c73ec3eb2cf39d30aaa0e611b42
Andrey Reznikov <reznikovandrusha@gmail.com>
2023-01-13 21:33:32 +0500

    fix: refactor solution with arrow fn

--- modules/10-basics/45-anonymous-functions/index.ts
+++ modules/10-basics/45-anonymous-functions/index.ts
@@ -1,11 +1,7 @@
 const numbers = [1, 3, 8, 9, 100, 23, 55, 34];
 
 // BEGIN
-function getEvenNumbers(): number[] {
-  const evenNumbers = numbers.filter((n) => n % 2 === 0);
-
-  return evenNumbers;
-}
+const getEvenNumbers = () => numbers.filter((num) => num % 2 === 0);
 // END
 
 export default getEvenNumbers;
