f36e3c064f55f325f77c868df39c28f87cf7037d
Арсений Конколович <333@onlinepbx.ru>
2023-01-13 18:33:42 +0500

    Fix marks

--- modules/25-types/20-union-types/test.ts
+++ modules/25-types/20-union-types/test.ts
@@ -1,17 +1,17 @@
 // import { assert } from 'type-assertions';
 // import type { Equal } from 'type-assertions';
 
-import lastIndex from "./index";
+import lastIndex from './index';
 
-test("lastIndex", () => {
+test('lastIndex', () => {
   // https://github.com/sindresorhus/is
   // https://github.com/unional/type-plus
   // https://github.com/colinhacks/zod
   // assert<Equal<ReturnType<typeof lastIndex>, number | null>>();
 
-  const str = "jest";
-  expect(lastIndex(str, "j")).toBe(0);
-  expect(lastIndex(str, "t")).toBe(3);
-  expect(lastIndex(str, "e")).toBe(1);
-  expect(lastIndex(str, "p")).toBeNull();
+  const str = 'jest';
+  expect(lastIndex(str, 'j')).toBe(0);
+  expect(lastIndex(str, 't')).toBe(3);
+  expect(lastIndex(str, 'e')).toBe(1);
+  expect(lastIndex(str, 'p')).toBeNull();
 });
