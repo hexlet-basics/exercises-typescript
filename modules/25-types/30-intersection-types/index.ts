3e6eac0323d528c1e83b406d773590cecf023e2c
Max <max@mail.ru>
2023-01-27 18:19:25 +0500

    add task for intersection types lesson

--- modules/25-types/30-intersection-types/index.ts
+++ modules/25-types/30-intersection-types/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+export type Person = {
+  name: string,
+  surname: string,
+  age: number,
+};
+
+export type Worker = Person & {
+  post: string,
+  salary: number,
+};
--- modules/25-types/30-intersection-types/test.ts
+++ modules/25-types/30-intersection-types/test.ts
@@ -1,3 +1,18 @@
-test('function', () => {
-  expect(1).toBe(1);
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Person, Worker } from '.';
+
+test('IntersectionTypes', () => {
+  const person: Person = {
+    name: 'Jim',
+    surname: 'Greeg',
+    age: 25,
+  };
+
+  const worker: Worker = {
+    name: 'Jim',
+    surname: 'Greeg',
+    age: 25,
+    post: 'Developer',
+    salary: 100000,
+  };
 });
