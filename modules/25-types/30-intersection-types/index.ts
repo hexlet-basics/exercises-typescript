a3e12e79ce9d8783a18b0c22949436ee1de27845
bondiano <batbondik0@gmail.com>
2023-02-07 19:46:56 +0600

    add exercise to intersection types lesson

--- modules/25-types/30-intersection-types/index.ts
+++ modules/25-types/30-intersection-types/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello, World!');
+type SinglyLinkedList = {
+    value: unknown;
+    next: SinglyLinkedList | null;
+}
+
+// BEGIN
+type DoubleLinkedList = SinglyLinkedList & {
+    prev: DoubleLinkedList | null;
+    next: DoubleLinkedList | null;
+}
+
+const reverseDoubleLinkedList = (head: DoubleLinkedList): void => {
+    let current: DoubleLinkedList | null = head;
+    let prev = null;
+    let next = null;
+
+    while (current) {
+        next = current.next;
+        prev = current.prev;
+
+        current.next = prev;
+        current.prev = next;
+
+        prev = current;
+        current = next;
+    }
+}
+// END
+
+export { DoubleLinkedList };
+export default reverseDoubleLinkedList;
--- modules/25-types/30-intersection-types/test.ts
+++ modules/25-types/30-intersection-types/test.ts
@@ -1,3 +1,33 @@
-test('function', () => {
-  expect(1).toBe(1);
+import reverseDoubleLinkedList, { DoubleLinkedList } from './index';
+
+test('reverseDoubleLinkedList', () => {
+  const list: DoubleLinkedList = {
+    value: 1,
+    next: null,
+    prev: null,
+  };
+
+  reverseDoubleLinkedList(list);
+  expect(list).toEqual(list);
+
+  const list2: DoubleLinkedList = {
+    value: 2,
+    next: null,
+    prev: list,
+  }
+  list.next = list2;
+
+  const list3: DoubleLinkedList = {
+    value: 3,
+    prev: list2,
+    next: null,
+  }
+  list2.next = list3;
+
+  reverseDoubleLinkedList(list);
+
+  expect(list.next).toBe(null);
+  expect(list.prev).toBe(list2);
+  expect(list2.next).toBe(list);
+  expect(list2.prev).toBe(list3);
 });
