2f98295ce7b723f5b7c5005309b3c20ca58e9167
bondiano <batbondik0@gmail.com>
2023-02-10 18:01:24 +0600

    fix typos

--- modules/25-types/40-assignability/test.ts
+++ modules/25-types/40-assignability/test.ts
@@ -3,13 +3,10 @@ import * as ta from 'type-assertions';
 import form from './index';
 
 test('form', () => {
-  expect(form).toBeDefined();
-
   const nameValidator = form.name.validator;
   const ageValidator = form.age.validator;
 
   expect(nameValidator(form.name.value)).toBe(true);
-
   expect(ageValidator(form.age.value)).toBe(false);
 
   ta.assert<ta.Equal<Parameters<typeof nameValidator>, [string]>>();
