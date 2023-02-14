cc1154634306aa6c5d6417ea3298e2d719f1f6cf
bondiano <batbondik0@gmail.com>
2023-02-14 20:47:46 +0600

    add structual typing exercise

--- modules/25-types/60-structural-typing/index.ts
+++ modules/25-types/60-structural-typing/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello, World!');
+// BEGIN
+enum LoadingStatus {
+  loading = 'loading',
+  success = 'success',
+  error = 'error',
+}
+
+type DataState =
+  | { status: LoadingStatus.loading }
+  | { status: LoadingStatus.success; data: number }
+  | { status: LoadingStatus.error; error: Error };
+
+const handleData = (dataState: DataState) => {
+  switch (dataState.status) {
+    case LoadingStatus.loading:
+      return 'loading...';
+    case LoadingStatus.success:
+      return String(dataState.data);
+    case LoadingStatus.error:
+      return dataState.error.message;
+    default:
+      return 'unknown';
+  }
+};
+// END
+
+export { DataState, LoadingStatus };
+export default handleData;
--- modules/25-types/60-structural-typing/test.ts
+++ modules/25-types/60-structural-typing/test.ts
@@ -1,3 +1,16 @@
-test('function', () => {
-  expect(1).toBe(1);
+import handleData, { DataState, LoadingStatus } from './index';
+
+test('handleData', () => {
+  const loading: DataState = { status: LoadingStatus.loading };
+  expect(handleData(loading)).toBe('loading...');
+
+  const success: DataState = { status: LoadingStatus.success, data: 42 };
+  expect(handleData(success)).toBe('42');
+
+  const error: DataState = { status: LoadingStatus.error, error: new Error('error') };
+  expect(handleData(error)).toBe('error');
+
+  const unknown = { status: 'unknown' };
+  // @ts-expect-error type '{ status: 'unknown' }' is not assignable to type 'DataState'.
+  expect(handleData(unknown)).toBe('unknown');
 });
