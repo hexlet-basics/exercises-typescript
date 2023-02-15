9d77fffe456ed3808a2fe28fc02231c9483bb398
bondiano <batbondik0@gmail.com>
2023-02-15 18:02:11 +0600

    update variability lesson and exercise

--- modules/25-types/70-variability/index.ts
+++ modules/25-types/70-variability/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello, World!');
+// BEGIN
+type Transaction = {
+  apply: (amount: number) => number;
+};
+
+type Wallet = {
+  transactions: Transaction[];
+  balance: number;
+};
+
+const applyTransactions = (wallet: Wallet) => {
+  try {
+    let balance = wallet.balance;
+    for (const transaction of wallet.transactions) {
+      balance = transaction.apply(balance);
+    }
+    return balance;
+  } catch (e) {
+    return wallet.balance;
+  }
+};
+// END
+
+export type { Transaction, Wallet };
+export default applyTransactions;
--- modules/25-types/70-variability/test.ts
+++ modules/25-types/70-variability/test.ts
@@ -1,3 +1,30 @@
-test('function', () => {
-  expect(1).toBe(1);
+import * as ta from "type-assertions";
+import applyTransactions, { Transaction, Wallet } from "./index";
+
+test("applyTransactions", () => {
+  const wallet: Wallet = {
+    balance: 100,
+    transactions: [
+      (amount: number) => amount + 10,
+      (amount: number) => amount - 20,
+      (amount: number) => amount + 30,
+    ],
+  };
+
+  expect(applyTransactions(wallet)).toBe(110);
+
+  const wallet2: Wallet = {
+    balance: 10,
+    transactions: [
+      (amount: number) => amount + 10,
+      () => {
+        throw new Error("Error");
+      },
+      (amount: number) => amount + 30,
+    ],
+  };
+
+  expect(applyTransactions(wallet2)).toBe(10);
+
+  ta.assert<ta.Equal<Parameters<Transaction["apply"]>, [number]>>();
 });
