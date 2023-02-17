ff86519397babe4339895f6618a2210804160335
bondiano <batbondik0@gmail.com>
2023-02-17 13:45:07 +0600

    fix linter

--- modules/30-classes/10-class-fields/index.ts
+++ modules/30-classes/10-class-fields/index.ts
@@ -1,7 +1,9 @@
 // BEGIN
 class File {
   name: string;
+
   size: number;
+
   constructor(name: string, size: number) {
     this.name = name;
     this.size = size;
--- modules/30-classes/10-class-fields/test.ts
+++ modules/30-classes/10-class-fields/test.ts
@@ -1,9 +1,9 @@
-import File from "./index";
+import File from './index';
 
-test("File", () => {
-  const file = new File("foo.txt", 4);
+test('File', () => {
+  const file = new File('foo.txt', 4);
 
-  expect(file.name).toBe("foo.txt");
+  expect(file.name).toBe('foo.txt');
   expect(file.size).toBe(4);
-  expect(file.toString()).toBe("foo.txt (4 bytes)");
+  expect(file.toString()).toBe('foo.txt (4 bytes)');
 });
