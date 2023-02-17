cee384230d98bbefe2f11fb16f2d221731653ac5
bondiano <batbondik0@gmail.com>
2023-02-17 15:10:29 +0600

    add class as types exercise

--- modules/30-classes/15-class-as-types/index.ts
+++ modules/30-classes/15-class-as-types/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello, World!');
+// BEGIN
+class File {
+  name: string;
+
+  size: number;
+
+  isCopy?: boolean;
+
+  constructor(options: File) {
+    this.name = options.name;
+    this.size = options.size;
+
+    if (options instanceof File) {
+      this.isCopy = true;
+    }
+  }
+
+  toString() {
+    return [this.isCopy && '(copy)', this.name, `(${this.size} bytes)`].filter(Boolean).join(' ');
+  }
+}
+// END
+
+export default File;
--- modules/30-classes/15-class-as-types/test.ts
+++ modules/30-classes/15-class-as-types/test.ts
@@ -1,3 +1,12 @@
-test('function', () => {
-  expect(1).toBe(1);
+import File from './index';
+
+test('File', () => {
+  const file = new File({ name: 'foo.txt', size: 4000 });
+
+  expect(file.name).toBe('foo.txt');
+  expect(file.size).toBe(4000);
+  expect(file.toString()).toBe('foo.txt (4000 bytes)');
+
+  const file2 = new File(file);
+  expect(file2.toString()).toBe('(copy) foo.txt (4000 bytes)');
 });
