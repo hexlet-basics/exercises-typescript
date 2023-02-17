49ffbd23ae925056423df65f1f157a3c4b3a48b8
bondiano <batbondik0@gmail.com>
2023-02-17 13:34:06 +0600

    add class fields exercise

--- modules/30-classes/10-class-fields/index.ts
+++ modules/30-classes/10-class-fields/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+// BEGIN
+class File {
+  name: string;
+  size: number;
+  constructor(name: string, size: number) {
+    this.name = name;
+    this.size = size;
+  }
+
+  toString() {
+    return `${this.name} (${this.size} bytes)`;
+  }
+}
+// END
+
+export default File;
--- modules/30-classes/10-class-fields/test.ts
+++ modules/30-classes/10-class-fields/test.ts
@@ -1,3 +1,9 @@
-test('function', () => {
-  expect(1).toBe(1);
+import File from "./index";
+
+test("File", () => {
+  const file = new File("foo.txt", 4);
+
+  expect(file.name).toBe("foo.txt");
+  expect(file.size).toBe(4);
+  expect(file.toString()).toBe("foo.txt (4 bytes)");
 });
