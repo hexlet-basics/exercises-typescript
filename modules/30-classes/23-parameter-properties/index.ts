6900e9bf9f0906a3877d62b7aa9fa49c6318bc7e
bondiano <batbondik0@gmail.com>
2023-02-19 14:18:51 +0600

    add class extending lesson

--- modules/30-classes/23-parameter-properties/index.ts
+++ modules/30-classes/23-parameter-properties/index.ts
@@ -2,10 +2,6 @@
 class File {
   constructor(private name: string, private size: number) { }
 
-  get sizeInKb() {
-    return this.size * 1024;
-  }
-
   toString() {
     return `${this.name} (${this.size} bytes)`;
   }
--- modules/30-classes/23-parameter-properties/test.ts
+++ modules/30-classes/23-parameter-properties/test.ts
@@ -4,9 +4,7 @@ test('File', () => {
   const file = new File('foo.txt', 4);
 
   expect(file.toString()).toBe('foo.txt (4 bytes)');
-  expect(file.sizeInKb).toBe(4096);
 
   const file2 = new File('bar.txt', 8);
   expect(file2.toString()).toBe('bar.txt (8 bytes)');
-  expect(file2.sizeInKb).toBe(8192);
 });
