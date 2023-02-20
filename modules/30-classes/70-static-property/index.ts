e291f69d6e7cdc11c8eaa67ab43e1771ac88d096
bondiano <batbondik0@gmail.com>
2023-02-20 13:13:39 +0600

    add static property lesson content

--- modules/30-classes/70-static-property/index.ts
+++ modules/30-classes/70-static-property/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello, World!');
+// BEGIN
+class UserResponse {
+  constructor(public user: string) {}
+
+  static fromArray(users: string[]): UserResponse[] {
+    return users.map((user) => new UserResponse(user));
+  }
+}
+// END
+
+export default UserResponse;
--- modules/30-classes/70-static-property/test.ts
+++ modules/30-classes/70-static-property/test.ts
@@ -1,3 +1,18 @@
-test('function', () => {
-  expect(1).toBe(1);
+import UserResponse from './index';
+
+test('UserResponse', () => {
+  const users = ['user1', 'user2'];
+  const result = UserResponse.fromArray(users);
+
+  expect(result).toEqual([
+    new UserResponse('user1'),
+    new UserResponse('user2'),
+  ]);
+  expect(result[0]).toBeInstanceOf(UserResponse);
+
+  const users2 = ['user3', 'user4', 'user5'];
+  const result2 = UserResponse.fromArray(users2);
+
+  expect(result2[0].user).toBe('user3');
+  expect(result2[2]).toBeInstanceOf(UserResponse);
 });
