b4218ac217bb759e4d8fa29b42ab01e7d7e14c1e
bondiano <batbondik0@gmail.com>
2023-02-20 20:23:07 +0600

    add abstract classes lesson

--- modules/30-classes/80-abstract-classes/index.ts
+++ modules/30-classes/80-abstract-classes/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello, World!');
+// BEGIN
+abstract class GameObject {
+  private movements: Array<[x: number, y: number]> = [];
+
+  constructor(protected x: number, protected y: number) {}
+
+  tick(delta: number): void {
+    const [x, y] = this.movements.shift() || [0, 0];
+    this.x += x * delta;
+    this.y += y * delta;
+  }
+
+  move(x: number, y: number): void {
+    this.movements.push([x, y]);
+  }
+
+  abstract render(): string;
+}
+// END
+
+export default GameObject;
--- modules/30-classes/80-abstract-classes/test.ts
+++ modules/30-classes/80-abstract-classes/test.ts
@@ -1,3 +1,53 @@
+/* eslint-disable max-classes-per-file */
+import GameObject from './index';
+
 test('function', () => {
-  expect(1).toBe(1);
+  class GameScene {
+    private objects: GameObject[] = [];
+
+    addObject(object: GameObject): void {
+      this.objects.push(object);
+    }
+
+    tick(delta: number): void {
+      this.objects.forEach((object) => object.tick(delta));
+    }
+
+    render(): string {
+      return this.objects.map((object) => object.render()).join('\n');
+    }
+  }
+
+  class Player extends GameObject {
+    render(): string {
+      return `Player: ${this.x}, ${this.y}`;
+    }
+  }
+
+  class Enemy extends GameObject {
+    render(): string {
+      return `Enemy: ${this.x}, ${this.y}`;
+    }
+  }
+
+  const scene = new GameScene();
+  const player = new Player(0, 0);
+  const enemy = new Enemy(20, 20);
+  scene.addObject(player);
+  scene.addObject(enemy);
+
+  player.move(10, 10);
+  enemy.move(20, 20);
+  player.move(10, 10);
+
+  scene.tick(1);
+  expect(scene.render()).toBe('Player: 10, 10\nEnemy: 40, 40');
+
+  scene.tick(0.5);
+  expect(scene.render()).toBe('Player: 15, 15\nEnemy: 40, 40');
+
+  enemy.move(-10, 10);
+  player.move(-5, 0);
+  scene.tick(1);
+  expect(scene.render()).toBe('Player: 10, 15\nEnemy: 30, 50');
 });
