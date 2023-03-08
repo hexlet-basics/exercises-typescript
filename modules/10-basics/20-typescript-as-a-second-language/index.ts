2012431a861683ad51f49ea19e926be022861d9b
sseezov <sseezov@mail.ru>
2023-03-08 13:50:46 +0700

    fix: добавил запятую, убрал тавталогию, исправил название

--- modules/10-basics/20-typescript-as-a-second-language/index.ts
+++ modules/10-basics/20-typescript-as-a-second-language/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 // BEGIN
-function multi(a: number, b: number) {
+function multiply(a: number, b: number) {
   return a * b;
 }
 // END
 
-export default multi;
+export default multiply;
--- modules/10-basics/20-typescript-as-a-second-language/test.ts
+++ modules/10-basics/20-typescript-as-a-second-language/test.ts
@@ -1,9 +1,9 @@
 import * as ta from 'type-assertions';
 
-import multi from './index';
+import multiply from './index';
 
-test('multi', () => {
-  expect(multi(1, 3)).toBe(3);
+test('multiply', () => {
+  expect(multiply(1, 3)).toBe(3);
 
-  ta.assert<ta.Equal<ReturnType<typeof multi>, number>>();
+  ta.assert<ta.Equal<ReturnType<typeof multiply>, number>>();
 });
