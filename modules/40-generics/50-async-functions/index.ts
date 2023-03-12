f336ff5dcdcd5b6ad41ae47c40f210e1e054ee2a
bondiano <batbondik0@gmail.com>
2023-03-12 21:34:36 +0600

    add generic async functions lesson

--- modules/40-generics/50-async-functions/index.ts
+++ modules/40-generics/50-async-functions/index.ts
@@ -1,1 +1,12 @@
-console.log('lala');
+// BEGIN
+const asyncMap = async <T, P>(arr: Promise<T>[], fn: (item: T) => P) => {
+  const promise = arr.map(async (item) => {
+    const result = await item;
+    return fn(result);
+  });
+
+  return Promise.all(promise);
+};
+// END
+
+export default asyncMap;
--- modules/40-generics/50-async-functions/test.ts
+++ modules/40-generics/50-async-functions/test.ts
@@ -1,3 +1,13 @@
-test('function', () => {
-  expect(1).toBe(1);
+import * as ta from 'type-assertions';
+import asyncMap from './index';
+
+test('asyncMap', async () => {
+  const result = await asyncMap([Promise.resolve(1), Promise.resolve(2)], (item) => item * 2);
+  expect(result).toEqual([2, 4]);
+
+  const result2 = await asyncMap([Promise.resolve('one'), Promise.resolve('two'), Promise.resolve('three')], (item) => item.toUpperCase());
+  expect(result2).toEqual(['ONE', 'TWO', 'THREE']);
+
+  ta.assert<ta.Equal<typeof result, number[]>>();
+  ta.assert<ta.Equal<typeof result2, string[]>>();
 });
