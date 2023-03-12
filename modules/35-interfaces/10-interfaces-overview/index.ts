336b581644bdf5da5b33c37c60528d2995c16bf0
sseezov <sseezov@mail.ru>
2023-03-12 18:06:41 +0700

    feat: добавил упражнение, дополнил теорию

--- modules/35-interfaces/10-interfaces-overview/index.ts
+++ modules/35-interfaces/10-interfaces-overview/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello, World!');
+interface IVehicle {
+  honk(): void;
+  seats: number;
+  colour: string;
+  canHavePassengers: boolean;
+  countKilometresTravelled(): number;
+}
+
+// BEGIN
+
+// END
+
+const porche = new Car();
+porche.honk();
--- modules/35-interfaces/10-interfaces-overview/test.ts
+++ modules/35-interfaces/10-interfaces-overview/test.ts
@@ -1,3 +1,8 @@
-test('function', () => {
-  expect(1).toBe(1);
+import * as path from 'path';
+import * as sinon from 'sinon';
+
+test('beep', async () => {
+  const spy = sinon.spy(console, 'log');
+  await import(path.join(__dirname, 'index'));
+  sinon.assert.calledWith(spy, sinon.match.string);
 });
