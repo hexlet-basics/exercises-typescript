9e96ccbc75c8d32b050913b3ac7871c6bc652e88
sseezov <sseezov@mail.ru>
2023-03-12 18:16:43 +0700

    fix:исправил упражнение

--- modules/35-interfaces/10-interfaces-overview/index.ts
+++ modules/35-interfaces/10-interfaces-overview/index.ts
@@ -7,7 +7,23 @@ interface IVehicle {
 }
 
 // BEGIN
+class Car implements IVehicle {
+  honk() {
+    console.log('beep beep!');
+  }
 
+  maxSpeed = '200 km/h';
+
+  seats = 4;
+
+  colour = 'black';
+
+  canHavePassengers = true;
+
+  countKilometresTravelled() {
+    return 10;
+  }
+}
 // END
 
 const porche = new Car();
