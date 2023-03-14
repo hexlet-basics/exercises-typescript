0bb8695194562e65c52e88cd5c93929a77132267
sseezov <sseezov@mail.ru>
2023-03-14 13:46:00 +0700

    fix:исправил на сокращенный синтаксис

--- modules/35-interfaces/10-interfaces-overview/index.ts
+++ modules/35-interfaces/10-interfaces-overview/index.ts
@@ -8,25 +8,12 @@ interface IVehicle {
 
 // BEGIN
 class Car implements IVehicle {
-  seats: number;
-
-  colour: string;
-
-  canHavePassengers: boolean;
-
-  fuelPer100Kilometers: number;
-
   constructor(
-    seats: number,
-    colour: string,
-    canHavePassengers: boolean,
-    fuelPer100Kilometers: number,
-  ) {
-    this.seats = seats;
-    this.colour = colour;
-    this.canHavePassengers = canHavePassengers;
-    this.fuelPer100Kilometers = fuelPer100Kilometers;
-  }
+    public seats: number,
+    public colour: string,
+    public canHavePassengers: boolean,
+    public fuelPer100Kilometers: number,
+  ) {}
 
   calcFuelNeeded(distance: number) {
     return (this.fuelPer100Kilometers / 100) * distance;
