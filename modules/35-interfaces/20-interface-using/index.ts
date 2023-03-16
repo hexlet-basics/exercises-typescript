d7fd5224faf5b8a55b6c9ce2cd74cb807d6b3f4e
sseezov <sseezov@mail.ru>
2023-03-16 09:45:22 +0700

    feat: 42 урок

--- modules/35-interfaces/20-interface-using/index.ts
+++ modules/35-interfaces/20-interface-using/index.ts
@@ -1,1 +1,33 @@
-console.log('Hello, World!');
+interface IFlying {
+  canFly: true;
+}
+
+interface IBird extends IFlying {
+  isLiving: true;
+}
+
+interface IPlane extends IFlying {
+  canCarryPeople: true;
+}
+interface ISuperman extends
+// BEGIN
+  IBird, IPlane {
+  guessWho: (guess: string) => void;
+}
+// END
+
+const superMan: ISuperman = {
+  canFly: true,
+  isLiving: true,
+  canCarryPeople: true,
+  // BEGIN
+  guessWho: (guess) => {
+    console.log(`It's a ${guess}?`);
+    return guess.toLowerCase() !== 'superman' ? console.log('No!') : console.log("It's a superman!");
+  },
+  // END
+};
+
+superMan.guessWho('bird');
+superMan.guessWho('plane');
+superMan.guessWho('superman');
--- modules/35-interfaces/20-interface-using/test.ts
+++ modules/35-interfaces/20-interface-using/test.ts
@@ -1,3 +1,13 @@
-test('function', () => {
-  expect(1).toBe(1);
+import * as path from 'path';
+import * as sinon from 'sinon';
+
+test('guess who', async () => {
+  const spy = sinon.spy(console, 'log');
+  await import(path.join(__dirname, 'index'));
+  sinon.assert.calledWith(spy, "It's a bird?");
+  sinon.assert.calledWith(spy, 'No!');
+  sinon.assert.calledWith(spy, "It's a plane?");
+  sinon.assert.calledWith(spy, 'No!');
+  sinon.assert.calledWith(spy, "It's a superman?");
+  sinon.assert.calledWith(spy, "It's a superman!");
 });
