5e90589cdf7392ad68df9f8cdb20c3a881cb218c
bondiano <batbondik0@gmail.com>
2023-03-24 14:25:29 +0600

    add generic classes lesson

--- modules/40-generics/60-generic-classes/index.ts
+++ modules/40-generics/60-generic-classes/index.ts
@@ -1,1 +1,18 @@
-console.log('lala');
+// BEGIN
+class Queue<T> {
+  private data: T[] = [];
+
+  enqueue(element: T): void {
+    this.data.push(element);
+  }
+
+  dequeue(): T {
+    if (this.data.length === 0) {
+      throw new Error('Queue is empty');
+    }
+    return this.data.shift() as T;
+  }
+}
+// END
+
+export default Queue;
--- modules/40-generics/60-generic-classes/test.ts
+++ modules/40-generics/60-generic-classes/test.ts
@@ -1,3 +1,19 @@
-test('function', () => {
-  expect(1).toBe(1);
+import Queue from './index';
+
+test('Queue', () => {
+  const queue = new Queue<number>();
+  queue.enqueue(1);
+  queue.enqueue(2);
+  queue.enqueue(3);
+  expect(queue.dequeue()).toBe(1);
+  expect(queue.dequeue()).toBe(2);
+  expect(queue.dequeue()).toBe(3);
+  expect(() => queue.dequeue()).toThrow();
+
+  const queue2 = new Queue<string>();
+  queue2.enqueue('one');
+  queue2.enqueue('two');
+  expect(queue2.dequeue()).toBe('one');
+  expect(queue2.dequeue()).toBe('two');
+  expect(() => queue2.dequeue()).toThrow();
 });
