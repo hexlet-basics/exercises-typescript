d641eb56e93a9b4121e66d464ffd2de4825b8321
sseezov <sseezov@mail.ru>
2023-04-03 12:44:23 +0700

    feat:добавил 43 урок

--- modules/35-interfaces/30-interface-implementation/index.ts
+++ modules/35-interfaces/30-interface-implementation/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello, World!');
+type Entry = {
+  [key: string]: number
+};
+
+interface IPhonebook {
+  entries: Entry
+  get(key: string): number | null
+  set(key: string, value: number): void
+}
+
+// BEGIN
+export default class Phonebook implements IPhonebook {
+  entries: Entry = {};
+
+  get(key: string): number | null {
+    return key in this.entries ? this.entries[key] : null;
+  }
+
+  set(key: string, value: number): void {
+    this.entries[key] = value;
+  }
+}
+// END
