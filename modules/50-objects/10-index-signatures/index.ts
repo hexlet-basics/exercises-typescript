8b242c6a80e668ad84400ff910a6049195c52c46
bondiano <batbondik0@gmail.com>
2023-05-22 00:25:19 +0600

    add index sugnatures lesson

--- modules/50-objects/10-index-signatures/index.ts
+++ modules/50-objects/10-index-signatures/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello, World!');
+interface SalaryStatistics {
+  min: number;
+  max: number;
+  avg: number;
+}
+
+// BEGIN
+export interface EmployeeSalary {
+  [name: string]: number;
+}
+
+export const buildSalaryStatistics = (employees: EmployeeSalary): SalaryStatistics => {
+  const salaries = Object.values(employees);
+  const min = Math.min(...salaries);
+  const max = Math.max(...salaries);
+  const avg = salaries.reduce((acc, salary) => acc + salary, 0) / salaries.length;
+
+  return { min, max, avg };
+};
+// END
--- modules/50-objects/10-index-signatures/test.ts
+++ modules/50-objects/10-index-signatures/test.ts
@@ -1,3 +1,24 @@
-test('function', () => {
-  expect(1).toBe(1);
+import { buildSalaryStatistics, EmployeeSalary } from '.';
+
+test('buildSalaryStatistics', () => {
+  const employees: EmployeeSalary = {
+    mango: 100,
+    poly: 50,
+    ajax: 150,
+  };
+
+  const expected = {
+    min: 50,
+    max: 150,
+    avg: 100,
+  };
+
+  expect(buildSalaryStatistics(employees)).toEqual(expected);
+
+  employees.ironMan = 1000;
+  expect(buildSalaryStatistics(employees)).toEqual({
+    min: 50,
+    max: 1000,
+    avg: 325,
+  });
 });
