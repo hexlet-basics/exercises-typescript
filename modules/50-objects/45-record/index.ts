ecbfe3413b267fbb92f0f875e89786ba67ebd5c3
Ivan Gagarinov <dzencot@gmail.com>
2023-07-27 17:32:35 +0500

    fix

--- modules/50-objects/45-record/index.ts
+++ modules/50-objects/45-record/index.ts
@@ -1,6 +1,8 @@
 // BEGIN
+type Property = string | number | symbol;
+
 const createAccessChecker = (
-  <Roles extends string | number | symbol, Resource>(permissions: Record<Roles, Array<Resource>>) => (
+  <Roles extends Property, Resource>(permissions: Record<Roles, Array<Resource>>) => (
     (role: Roles, resource: Resource) => permissions[role].includes(resource)
   )
 );
