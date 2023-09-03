f52ee2db4fa3c6b0901d3a3c7b58a2bb9693ee44
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-09-03 15:20:05 +0300

    [#263] rename var from singular to plural form in teacher solution at async fn lesson

--- modules/40-generics/50-async-functions/index.ts
+++ modules/40-generics/50-async-functions/index.ts
@@ -1,11 +1,11 @@
 // BEGIN
 const asyncMap = async <T, P>(arr: Promise<T>[], fn: (item: T, index: number) => P) => {
-  const promise = arr.map(async (item, index) => {
+  const promises = arr.map(async (item, index) => {
     const result = await item;
     return fn(result, index);
   });
 
-  return Promise.all(promise);
+  return Promise.all(promises);
 };
 // END
 
