c4f8a9d0abca18a436313cb315c926499ea83997
ola-9 <nepogodda@gmail.com>
2023-10-25 13:25:01 +0300

    update tuples tests

--- modules/22-arrays/50-tuples/test.ts
+++ modules/22-arrays/50-tuples/test.ts
@@ -4,7 +4,11 @@ test('function', () => {
   const p1: Point = [1, 2, 3];
   const p2: Point = [1, 2, 3];
   const p3: Point = [0, 2, 3];
+  const p4: Point = [1, 21, 3]; 
+  const p5: Point = [1, 2, 13];
+
 
   expect(isTheSamePoint(p1, p2)).toBe(true);
   expect(isTheSamePoint(p1, p3)).toBe(false);
+  expect(isTheSamePoint(p4, p5)).toBe(false);
 });
