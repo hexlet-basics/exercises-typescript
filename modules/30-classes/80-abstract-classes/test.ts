7d33350557cd13ebce8a45028a3aafd3c77bac44
bondiano <batbondik0@gmail.com>
2023-10-25 12:46:46 +0700

    fix 4th module after editor review

--- modules/30-classes/80-abstract-classes/test.ts
+++ modules/30-classes/80-abstract-classes/test.ts
@@ -4,8 +4,12 @@ import Clock from './index';
 test('GameObject', () => {
   class Clock12 extends Clock {
     render(): string {
-      const timeType = this.hours >= 12 ? 'PM' : 'AM';
-      const currentHour = this.hours % 12;
+      const timeType = this.hours > 12 ? 'PM' : 'AM';
+
+      let currentHour = this.hours > 12 ? this.hours - 12 : this.hours;
+      if (timeType === 'AM' && this.hours === 0) {
+        currentHour = 12;
+      }
 
       const hours = currentHour.toString().padStart(2, '0');
       const minutes = this.minutes.toString().padStart(2, '0');
@@ -20,7 +24,7 @@ test('GameObject', () => {
   }
 
   const clock121 = new Clock12(12, 59, 0);
-  expect(clock121.render()).toBe('00 : 59 PM');
+  expect(clock121.render()).toBe('12 : 59 AM');
 
   const clock12 = new Clock12(23, 59, 58);
   expect(clock12.render()).toBe('11 : 59 PM');
@@ -28,12 +32,13 @@ test('GameObject', () => {
   clock12.tick();
   clock12.tick();
 
+  expect(clock12.render()).toBe('12 : 00 AM');
+
   const clock24 = new Clock24(23, 59, 58);
   expect(clock24.render()).toBe('23 : 59');
 
   clock24.tick();
   clock24.tick();
 
-  expect(clock12.render()).toBe('00 : 00 AM');
   expect(clock24.render()).toBe('00 : 00');
 });
