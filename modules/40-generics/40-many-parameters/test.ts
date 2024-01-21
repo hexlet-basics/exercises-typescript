ddfa1c291718a570e25a74ddf61fc00e38b3c7ae
Vasilii Boldurean <vasilii.boldurean@computacenter.com>
2024-01-21 19:55:42 +0000

    Fix: Map set method should return Map instance in order to chain operations.

--- modules/40-generics/40-many-parameters/test.ts
+++ modules/40-generics/40-many-parameters/test.ts
@@ -1,23 +1,26 @@
-import * as ta from 'type-assertions';
+import * as ta from "type-assertions";
 
-import MyMap from './index';
+import MyMap from "./index";
 
-test('MyMap', () => {
+test("MyMap", () => {
   const map: MyMap<string, number> = {
     values: new Map(),
     set(key, value) {
       this.values.set(key, value);
+      return this;
     },
     get(key) {
       return this.values.get(key);
     },
   };
 
-  map.set('one', 1);
-  map.set('two', 2);
+  map.set("one", 1);
+  map.set("two", 2);
 
-  expect(map.get('one')).toBe(1);
-  expect(map.get('two')).toBe(2);
+  expect(map.get("one")).toBe(1);
+  expect(map.get("two")).toBe(2);
 
-  ta.assert<ta.Equal<Parameters<MyMap<string, string>['set']>, [string, string]>>();
+  ta.assert<
+    ta.Equal<Parameters<MyMap<string, string>["set"]>, [string, string]>
+  >();
 });
