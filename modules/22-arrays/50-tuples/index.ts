a40e841eacf9a5967a39d51a24fa4540292efacc
Olga <ipvola@gmail.com>
2026-04-16 17:57:11 -0400

    add:check for an empty arr

--- modules/22-arrays/50-tuples/index.ts
+++ modules/22-arrays/50-tuples/index.ts
@@ -2,6 +2,10 @@
 export type Point = [number, number, number];
 
 function isTheSamePoint(p1: Point, p2: Point): boolean {
+  if (p1.length !== p2.length) {
+    return false;
+  }
+  
   return p1.every((el, i) => el === p2[i]);
 }
 // END
